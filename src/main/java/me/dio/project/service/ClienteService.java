f83ea70ad9380aeac116714d3692f1cb261d7bf6
jordanclaudino <jordanclaudino@gmail.com>
2022-10-29 16:52:57 -0300

    Second Commit

--- src/main/java/me/dio/project/service/ClienteService.java
+++ src/main/java/me/dio/project/service/ClienteService.java
@@ -1,4 +1,15 @@
 package me.dio.project.service;
 
+import me.dio.project.model.Cliente;
+
 public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
 }
